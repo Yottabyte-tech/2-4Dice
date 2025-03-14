f931bb17519db75876d46495f5e61863a211505f
PeriodTerminal <callanchem314@gmail.com>
2025-03-13 18:53:35 -0700

    'update'

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import './App.css'
 import Game from './Game.jsx'
+import Header from './Header.jsx'
 
 function App() {
   return (
     <>
+      <Header />
       <Game />
     </>
   )
---
+++ src/Header.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function Header() {
+  return (
+    <>
+      <h1>2-4 Dice</h1>
+    </>
+  )
+}
+
+export default Header
