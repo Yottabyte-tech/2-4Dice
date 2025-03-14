952f180aa43096b400de5d5c654d83ac48e33ff7
PeriodTerminal <callanchem314@gmail.com>
2025-03-13 18:54:45 -0700

    'update'

--- src/Game.jsx
+++ src/Game.jsx
@@ -3,7 +3,6 @@ import './App.css'
 function Game() {
   return (
     <>
-      <h1>2-4 Dice</h1>
       <iframe scrolling="no" src="https://html-classic.itch.zone/html/13073362/index.html" width='1080' height='608'></iframe>
     </>
   )
